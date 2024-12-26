234d4a95839c60b6cb890c67841abd2c85d64747
Dennis Mattews <alttert@mail.ru>
2024-12-26 06:25:00 +0300

    improved tests

--- test_main.go
+++ test_main.go
@@ -1,12 +1,11 @@
 package GoThemePark
 
 import (
-	"GolangThemePark/GoThemePark"
 	"testing"
 )
 
 func TestCalculateRevenue(t *testing.T) {
-	revenue := GoThemePark.calculateRevenue(10, 20.5)
+	revenue := calculateRevenue(10, 20.5)
 	if revenue != 205.0 {
 		t.Errorf("Ожидалось 205.0, но получено %.2f", revenue)
 	}
@@ -14,12 +13,15 @@ func TestCalculateRevenue(t *testing.T) {
 
 func TestPromoteAttraction(t *testing.T) {
 	attraction := "Колесо обозрения"
-	GoThemePark.promoteAttraction(&attraction)
+	promoteAttraction(&attraction)
 	if attraction != "Колесо обозрения - САМЫЙ ПОПУЛЯРНЫЙ!" {
 		t.Errorf("Ожидалось 'Колесо обозрения - САМЫЙ ПОПУЛЯРНЫЙ!', но получено '%s'", attraction)
 	}
 }
 
-func TestHandleErrors(t *testing.T) {
-	GoThemePark.handleErrors(0, 0) // Протестируем с нулём
+func TestCheckCapacity(t *testing.T) {
+	err := checkCapacity(1200)
+	if err == nil {
+		t.Error("Ожидалась ошибка при превышении вместимости, но её не было")
+	}
 }
